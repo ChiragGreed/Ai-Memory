919d1bd7f6275d22af4cb5ce870a70144a92a0dc
Chirag Greed <chiraggreed02@gmail.com>
2026-04-02 17:27:33 +0530

    Update static file directory to serve from public/dist

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const publicFile = path.join(__dirname, "../", "public")
+const publicFile = path.join(__dirname, "../", "public/dist")
 
 app.use(express.static(publicFile))
 
